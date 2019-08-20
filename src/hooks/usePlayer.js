76b12db698d2ea07a95dbeeac8b8c2668aa49ccc
Bryce Eadie <bryce.eadie@gmail.com>
2019-08-20 14:07:59 -0700

    Develop game logic with custom hooks

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,12 +1,13 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 
-import { randomTetromino } from '../tetrominos';
+import { TETROMINOS, randomTetromino } from '../tetrominos';
+import { STAGE_WIDTH } from '../gameHelpers';
 
 export const usePlayer = () => {
     // short version
     const [player, setPlayer] = useState({
         pos: { x: 0, y: 0 },
-        tetromino: randomTetromino().shape,
+        tetromino: TETROMINOS[0].shape,
         collided: false
     })
 
@@ -15,5 +16,24 @@ export const usePlayer = () => {
     // const player = playerState[0];
     // const setPlayer = playerState[1];
 
-    return [player];
+
+    const updatePlayerPos = ({ x, y, collided }) => {
+        setPlayer((prev) => ({
+            ...prev,
+            pos: { x: (prev.pos.x += x), y: (prev.pos.y += y)},
+            collided
+        }))
+    }
+
+    // useCallback prevents infinite loop
+    const resetPlayer = useCallback(() => {
+        setPlayer({
+            pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
+            tetromino: randomTetromino().shape,
+            collided: false
+        })
+    }, [])
+
+   
+    return [player, updatePlayerPos, resetPlayer];
 }
