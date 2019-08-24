ea7265aead4b92255be10ccaf18585d0085c4ae7
Bryce Eadie <bryce.eadie@gmail.com>
2019-08-24 14:51:29 -0700

    Implement score and level tracking

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -9,6 +9,7 @@ import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 import { useInterval } from '../hooks/useInterval';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
+import { useGameStatus } from '../hooks/useGameStatus';
 
 // Components
 import Stage from './Stage';
@@ -21,7 +22,8 @@ const Tetris = () => {
     const [gameOver, setGameOver] = useState(false)
 
     const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer()
-    const [stage, setStage] = useStage(player, resetPlayer)
+    const [stage, setStage, rowsCleared] = useStage(player, resetPlayer)
+    const [score, setScore, rows, setRows, level, setLevel] = useGameStatus(rowsCleared)
 
     console.log('re-render')
 
@@ -34,13 +36,23 @@ const Tetris = () => {
 
     const startGame = () => {
         // Reset everything
-        setDropTime(1000)
         setStage(createStage())
+        setDropTime(1000)
         resetPlayer()
         setGameOver(false)
+        setScore(0)
+        setRows(0)
+        setLevel(0)
     }
 
     const drop = () => {
+        // Increase level if player has cleared 10 rows
+        if(rows > (level + 1) * 10) {
+            setLevel((prev) => prev + 1)
+            // Increase speed
+            setDropTime(1000 / (level + 1) + 200)
+        }
+
         if(!checkCollision(player, stage, { x: 0, y: 1 })) {
             updatePlayerPos({ x: 0, y: 1, collided: false  })
         } else {
@@ -55,16 +67,19 @@ const Tetris = () => {
         }
     }
 
-        // Reset drop time after player releases down key
+        // Reset drop time after player releases down key. 
+        // keyCode is destructured from event
     const keyUp = ({ keyCode }) => {
         if(!gameOver) {
             if(keyCode === 40) {
-                setDropTime(1000)
+                console.log('interval on')
+                setDropTime(1000 / (level + 1) + 200)
             }
         }
     }
 
     const dropPlayer = () => {
+        console.log('interval off')
         setDropTime(null)
         drop()
     }
@@ -105,9 +120,9 @@ const Tetris = () => {
                         <Display gameOver={gameOver} text="Game Over" />
                     ) : (
                         <div>
-                            <Display text="Score" />
-                            <Display text="Rows" />
-                            <Display text="Level" />
+                            <Display text={`Score: ${score}`} />
+                            <Display text={`Rows: ${rows}`} />
+                            <Display text={`Level: ${level}`} />
                         </div>
                     )}
                     <StartButton callback={startGame} />
