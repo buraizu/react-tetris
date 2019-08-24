ccf0aaa40f0ad84af136e67a8f5abf6676d4a93d
Bryce Eadie <bryce.eadie@gmail.com>
2019-08-24 00:43:09 -0700

    Implement movement of game board

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -6,6 +6,7 @@ import { createStage, checkCollision } from '../gameHelpers';
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
 
 // Custom Hooks
+import { useInterval } from '../hooks/useInterval';
 import { usePlayer } from '../hooks/usePlayer';
 import { useStage } from '../hooks/useStage';
 
@@ -33,6 +34,7 @@ const Tetris = () => {
 
     const startGame = () => {
         // Reset everything
+        setDropTime(1000)
         setStage(createStage())
         resetPlayer()
         setGameOver(false)
@@ -53,7 +55,17 @@ const Tetris = () => {
         }
     }
 
+        // Reset drop time after player releases down key
+    const keyUp = ({ keyCode }) => {
+        if(!gameOver) {
+            if(keyCode === 40) {
+                setDropTime(1000)
+            }
+        }
+    }
+
     const dropPlayer = () => {
+        setDropTime(null)
         drop()
     }
 
@@ -75,8 +87,17 @@ const Tetris = () => {
         }
     }
 
+    useInterval(() => {
+        drop()
+    }, dropTime)
+
     return (
-        <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={ (e) => move(e) }>
+        <StyledTetrisWrapper 
+            role="button" 
+            tabIndex="0" 
+            onKeyDown={ (e) => move(e) } 
+            onKeyUp={keyUp}
+        >
             <StyledTetris>
                 <Stage stage={stage} />
                 <aside>
